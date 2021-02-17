b601855dc52dc76be7c7ed1d344e75ebe87b32e5
Ingun Jon <ingun@foxxing.com>
2021-02-17 13:47:08 +0900

    update library url

--- test/test-data-provision.ts
+++ test/test-data-provision.ts
@@ -2,7 +2,7 @@ import {TestDataProvision} from "./provision-type";
 import {URL} from "url";
 
 export const testDataProvision: TestDataProvision = {
-    liburl: new URL("https://viewer-library.s3.ap-northeast-2.amazonaws.com/d.js"),
+    liburl: new URL("https://viewer-library.s3.ap-northeast-2.amazonaws.com/update-control.js"),
     srestURLs: [
         'https://viewer-test-model.s3.ap-northeast-2.amazonaws.com/srests/8da914c6-fe27-4dbf-9e89-a6e3f42bb72b/010074390d0848e0bb75184e53efebfa/srest.json',
         'https://viewer-test-model.s3.ap-northeast-2.amazonaws.com/srests/8da914c6-fe27-4dbf-9e89-a6e3f42bb72b/1d55ad72560142a486e6a97deebcbcbe/srest.json',
